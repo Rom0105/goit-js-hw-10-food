e781da1f30f7062e0f2ef2fc41e94fabc6ba57f4
Rom0105 <savosko.photo@gmail.com>
2021-06-09 18:13:48 +0300

    правки

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './sass/main.scss';
+import menus from './menu.json';
+import foodCard from '../src/templates/food-card.hbs';
+
+const foodRef = document.querySelector('.js-menu');
+const markup = foodCard(menus);
+foodRef.insertAdjacentHTML('beforeend', markup);
